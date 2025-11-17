3fae287e6d28e8295cee5793d086707e8d79437b
ASSINE20 <assine.geraud@gmail.com>
2025-11-17 16:24:16 +0100

    first commit

--- oneCatHandlers.go
+++ oneCatHandlers.go
@@ -2,7 +2,32 @@ package main
 
 import "net/http"
 
+
 func getCat(req *http.Request) (int, any) {
-	Logger.Info("Cat not found")
-	return http.StatusNotFound, "Cat not found"
+    catId := req.PathValue("catId")
+
+    cat, exists := catsDatabase[catId]
+    if !exists {
+        Logger.Infof("Cat '%s' not found", catId)
+        return http.StatusNotFound, "Cat not found"
+    }
+
+    return http.StatusOK, cat
+}
+
+
+func deleteCat(req *http.Request) (int, any) {
+    catId := req.PathValue("catId")
+
+    _, exists := catsDatabase[catId]
+    if !exists {
+        Logger.Infof("Cat '%s' not found", catId)
+        return http.StatusNotFound, "Cat not found"
+    }
+
+    delete(catsDatabase, catId)
+
+    return http.StatusOK, map[string]string{
+        "message": "Cat deleted",
+    }
 }
